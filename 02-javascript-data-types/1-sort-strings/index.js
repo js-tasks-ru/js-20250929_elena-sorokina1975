6456792f99961325db0a5922eac138451b0ccf39
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-10-10 15:53:56 +0300

    Upd Homework 2

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,19 +5,9 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-    let typeSort = {
-        asc: (a, b) => {
-            let k = a.localeCompare(b,['ru', 'en'], {caseFirst : 'upper'});
-            if (k > 0) return 1;
-            if (k == 0) return 0;
-            if (k < 0) return -1;
-        },
-        desc: (a, b) => {
-            let k = a.localeCompare(b,['ru', 'en'], {caseFirst : 'upper'});            
-            if (k > 0) return -1;
-            if (k == 0) return 0;
-            if (k < 0) return 1;
-        }
+    const typeSort = {
+        asc: (a, b) => a.localeCompare(b,['ru', 'en'], {caseFirst : 'upper'}),
+        desc: (a, b) => (-1)*a.localeCompare(b,['ru', 'en'], {caseFirst : 'upper'})
     }
     return [...arr].sort(typeSort[param]);
 }
