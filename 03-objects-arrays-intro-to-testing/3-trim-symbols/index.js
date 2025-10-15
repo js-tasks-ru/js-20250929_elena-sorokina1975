910aa648b0fce31933cdd4cc4c7ec101dcedea29
Elena Sorokina <elena-sorokina1975@yandex.ru>
2025-10-16 00:04:39 +0300

    Homework 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if ((string.length == 0) || (size <=0)) {
+    return '';
+  }
+  if (size == undefined) {
+    return string;
+  }
+  let result = string[0]; 
+  let countSymb = 1;
+  let s = string[0]; 
+  let sPrev = string[0]; 
+  for (let i = 1; i < string.length; i++) {
+      s = string[i];   
+      if (s == sPrev)  {
+          countSymb++;
+          if (countSymb <= size) {
+            result = result + s;
+          } else {
+            countSymb++;                  
+          }
+    } else {
+        result = result + s;
+        countSymb = 1;  
+    }
+    sPrev = string[i];  
+  }    
+  return result;  
 }
